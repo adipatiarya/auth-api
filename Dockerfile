099e44416f30f69c81e6c9fcc30cbb7a8d674d06
Kirun <jawircodes@gmail.com>
2023-02-19 02:16:43 +0700

    compose init

--- Dockerfile
+++ Dockerfile
@@ -19,4 +19,4 @@ COPY . .
 
 EXPOSE 5000
 
-CMD [ "npm", "run", "start:dev" ]
+CMD [ "npm", "run", "start" ]
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ http.createServer((req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/plain',
   });
+
   res.write('Hello, World!\n');
   res.end();
 }).listen(5000);
